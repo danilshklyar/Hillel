0d11b05b7c4c037c658596b9f7b0fe2ec6f367e2
danilshklyar <danili41989@gmail.com>
2021-02-06 12:27:19 +0200

    add lesson3 исключения

---
+++ lesson4.py
@@ -0,0 +1,24 @@
+# обработка исключений
+
+# value = input('Введи целое число')
+# if value.isdigit():
+#     value = int(value)
+#     result = value / 2
+#     print(result)
+# else:
+#     print("не верный ввод")
+
+value = input('Введи целое число')
+if value.isdigit():
+    value = int(value)
+    result = value / 2
+else:
+    print("не верный ввод")
+    result = 0
+
+print(result)
+
+# методы строк
+# цикл for
+# кортежи
+# списки
