3dc8d6870e7ca72652dcb58fcdf5b56457ca221d
danilshklyar <danili41989@gmail.com>
2021-02-24 12:16:14 +0200

    урок 6

--- Lesson6.py
+++ Lesson6.py
@@ -26,16 +26,16 @@
 my_list_1 = [1, 2, 3, 4, 5]
 my_list_2 = [10, 20, 30, 40, 50, 60, 70]
 
-my_result = []
-list_len = min(len(my_list_1), len(my_list_2)) # min выбирает список меньшей длинны из двух
-if len(my_list_2) < len(my_list_1):
-    my_list_2, my_list_1 = my_list_1, my_list_2
-for index in range(list_len):
-    my_result.append(my_list_1[index])
-    my_result.append(my_list_2[index])
-tail = my_list_2[list_len:]
-my_result += tail
-print(my_result)
+# my_result = []
+# list_len = min(len(my_list_1), len(my_list_2)) # min выбирает список меньшей длинны из двух
+# if len(my_list_2) < len(my_list_1):
+#     my_list_2, my_list_1 = my_list_1, my_list_2
+# for index in range(list_len):
+#     my_result.append(my_list_1[index])
+#     my_result.append(my_list_2[index])
+# tail = my_list_2[list_len:]
+# my_result += tail
+# print(my_result)
 #########################################
 # Смена значений местами
 # a = 5
